174433fe915452f15e847e642f4cdd09c353aed7
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-07-15 00:01:12 +0600

    add Dialog

--- apps/web/src/components/action-buttons.tsx
+++ apps/web/src/components/action-buttons.tsx
@@ -7,21 +7,47 @@ import {
     TooltipTrigger,
 } from '@/components/ui/tooltip';
 import { Button } from './ui/button';
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+} from '@/components/ui/dialog';
+import React, { useState } from 'react';
+
 const mapping: Array<{
     icon: React.ReactElement;
     tooltip: string;
+    dialog: {
+        title: string;
+        content: React.ReactElement;
+    };
 }> = [
     {
         icon: <Link2 strokeWidth={4} className="text-teal-500" />,
         tooltip: 'Add Torrent Link',
+        dialog: {
+            title: 'Add torrent links',
+            content: <></>,
+        },
     },
     {
         icon: <Plus strokeWidth={4.7} className="text-teal-500" />,
         tooltip: 'Add Torrent File',
+        dialog: {
+            title: '',
+            content: <></>,
+        },
     },
     {
         icon: <Settings strokeWidth={3} className="text-indigo-300" />,
         tooltip: 'Settings',
+        dialog: {
+            title: '',
+            content: <></>,
+        },
     },
 ];
 
@@ -30,21 +56,42 @@ export default function ActionButtons() {
         <div className="flex mb-4 border p-4 rounded-md">
             <div className="flex gap-5">
                 {mapping.map((item, index) => {
+                    const [dialogOpen, setDialogOpen] = useState(false);
                     return (
-                        <Tooltip key={index}>
-                            <TooltipTrigger asChild>
-                                <Button
-                                    className="cursor-pointer"
-                                    variant="outline"
-                                    size="icon"
-                                >
-                                    {item.icon}
-                                </Button>
-                            </TooltipTrigger>
-                            <TooltipContent>
-                                <p>{item.tooltip}</p>
-                            </TooltipContent>
-                        </Tooltip>
+                        <div key={index}>
+                            <Tooltip>
+                                <TooltipTrigger asChild>
+                                    <Button
+                                        className="cursor-pointer"
+                                        variant="outline"
+                                        size="icon"
+                                        onClick={() => {
+                                            setDialogOpen(true);
+                                        }}
+                                    >
+                                        {item.icon}
+                                    </Button>
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    <p>{item.tooltip}</p>
+                                </TooltipContent>
+                            </Tooltip>
+                            <Dialog
+                                open={dialogOpen}
+                                onOpenChange={setDialogOpen}
+                            >
+                                <DialogContent>
+                                    <DialogHeader>
+                                        <DialogTitle>
+                                            {item.dialog.title}
+                                        </DialogTitle>
+                                        <DialogDescription>
+                                            {item.dialog.content}
+                                        </DialogDescription>
+                                    </DialogHeader>
+                                </DialogContent>
+                            </Dialog>
+                        </div>
                     );
                 })}
             </div>
